90765c70677affc08073c43adab4c286cd21455d
Rafaela Perruci <rafaelaoperruci@gmail.com>
2025-05-14 19:31:27 -0300

    [Feat] Inclusão do README e configurações adicionais

---
+++ README.md
@@ -0,0 +1,8 @@
+# 🎬 Api de Consulta e Cadastro de Filmes
+
+## Funcionalidades:
+➕ Cadastrar novos filmes
+
+🔍 Consultar filmes por ID ou título
+
+📄 Listar todos os filmes cadastrados
---
+++ src/main/java/io/github/rafaelaperruci/moviecataloginfo_api/MovieCatalogInfoApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.rafaelaperruci.moviecataloginfo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogInfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogInfoApiApplication.class, args);
+	}
+
+}

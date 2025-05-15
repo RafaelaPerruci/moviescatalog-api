d0c63f9ba4bf2ca459669dee45073fc753fadc61
Rafaela Perruci <rafaelaoperruci@gmail.com>
2025-05-14 21:24:18 -0300

    [Feature] schema.sql adicionado

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,8 @@
-CREATE TABLE movies (
-                        id VARCHAR(36) PRIMARY KEY,
-                        title VARCHAR(255) NOT NULL,
-                        overview_resume VARCHAR(400),
-                        release_date VARCHAR(10),
-                        rating DECIMAL(1,1)
+CREATE TABLE movies
+(
+    id              VARCHAR(36) PRIMARY KEY,
+    title           VARCHAR(255) NOT NULL,
+    overview_resume VARCHAR(400),
+    release_date    VARCHAR(10),
+    rating          DECIMAL(1, 1)
 );
